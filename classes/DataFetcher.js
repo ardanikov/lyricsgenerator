788b261032857e3cb8490529fe64993e2d284edf
ardanikov <sess.uuai@gmail.com>
2026-06-22 14:33:30 +0700

    change : token

--- classes/DataFetcher.js
+++ classes/DataFetcher.js
@@ -13,7 +13,7 @@ class DataFetcher {
     async getSongInfos(name, limit = 1) {
         const query = name;
 
-        const apiKey = 'b362b9a7f5f0c5a7f749d568b68bc32a';
+        const apiKey = '34a340535bcf5efecbbcfa3d046c786a';
         const searchUrl = `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${encodeURIComponent(query)}&api_key=${apiKey}&format=json&limit=${limit}`;
 
         const response = await fetch(searchUrl);
@@ -26,7 +26,7 @@ class DataFetcher {
                 const infoUrl = `https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${apiKey}&artist=${encodeURIComponent(searchTrack.artist)}&track=${encodeURIComponent(searchTrack.name)}&format=json`;
                 const infoResponse = await fetch(infoUrl, {
                     headers: {
-                        'User-Agent': 'Application LyricPost/1.0 (pogromca.ap@gmail.com)',
+                        'User-Agent': 'Application Lyrics Post (sess.uuai@gmail.com)',
                     },
                 });
                 const infoResult = await infoResponse.json();
